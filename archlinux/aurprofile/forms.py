67a1de2c4d09c848be7e9a766d8c9af7ba1ed897
Laszlo Papp <djszapi@archlinux.us>
2009-11-23 00:42:23 +0100

    Fix some warnings/errors in aurprofile/forms.py
    Fix the following warnings/errors:
    
    * W:  5:ProfileUpdateForm.Meta: Class has no __init__ method
    * C:  4:ProfileUpdateForm: Missing docstring
    * C:  5:ProfileUpdateForm.Meta: Missing docstring
    * Implement better exception handling in this file
    
    Signed-off-by: Laszlo Papp <djszapi@archlinux.us>

--- archlinux/aurprofile/forms.py
+++ archlinux/aurprofile/forms.py
@@ -2,6 +2,22 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 class ProfileUpdateForm(ModelForm):
+    """
+    Some explanation for ProfileUpdateForm.
+
+    >>> form = ProfileUpdateForm( )
+    >>> form
+    u"some output"
+    >>> form.somemethod( )
+    u"more output"
+    """
     class Meta:
+        """
+        contains metadata about this class.
+        """
+        def __init__( self ):
+            raise Exception( "Shouldn't be instanciated" )
+
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
+
