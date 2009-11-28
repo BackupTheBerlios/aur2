a73808e21190571c5a50e0e2847a6bd9cea5d910
Laszlo Papp <djszapi@archlinux.us>
2009-11-28 09:33:04 +0100

    Refactor account registration, keep one account for one mail
    Refactor the account registration with the help of new version of
    django-registration module which provides facility for it with the
    RegistrationFormUniqueEmail element to reject those requirements to register an
    account in which cases an account was registered earlier for that email address
    
    The implementation touched the urls.py file to introduce the backend facility in
    the new module in he accounts and account/register regards.
    
    Signed-off-by: Laszlo Papp <djszapi@archlinux.us>

--- archlinux/urls.py
+++ archlinux/urls.py
@@ -2,14 +2,16 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
 import aur.forms
+from registration.forms import RegistrationFormUniqueEmail
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
-    (r'^accounts/register', 'registration.views.register', { 'form_class' :
-        aur.forms.AurRegistrationForm } ),
-    (r'^accounts/', include('registration.urls')),
+    (r'^accounts/register/$', 'registration.views.register',
+        {'backend': 'registration.backends.default.DefaultBackend',
+            'form_class': RegistrationFormUniqueEmail}),
+        (r'^accounts/', include('registration.backends.default.urls')),
     (r'^profile/', include('aurprofile.urls')),
     (r'^', include('aur.urls')),
 )
