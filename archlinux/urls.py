b0b76a778df14be6c6e016e3d891a2521647ac16
Laszlo Papp <djszapi@archlinux.us>
2009-12-06 11:04:16 +0100

    Implement RSS facility for newly updated/added packages
    This is similar that can be seen on so much site, like current AUR too.
    
    Implementation details:
    
    * New Feed class was established and therefore a new file too, aur/feeds.py
    * New entry was added in the urls.py to it, and a new feed element
    * The search template was modified in extra_head block regard, new link was
    * added with the related /feeds/rss url in a link
    
    Signed-off-by: Laszlo Papp <djszapi@archlinux.us>

--- archlinux/urls.py
+++ archlinux/urls.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from django.conf import settings
 import aurprofile.views
 
+from aur.feeds import RssLatestPackages, AtomLatestPackages
+
 admin.autodiscover()
+
+feeds = {
+    'rss': RssLatestPackages
+    # 'atom': AtomLatestPackages,
+}
 
 urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
@@ -14,6 +21,8 @@ urlpatterns = patterns('',
     (r'^profile/', include('aurprofile.urls')),
     (r'^', include('aur.urls')),
     (r'^openid/', include('django_openid_auth.urls')),
+    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed',
+        {'feed_dict': feeds}),
 )
 
 if settings.DEBUG == True:
