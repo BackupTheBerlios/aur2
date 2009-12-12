bd6d59bf311a7fc1a3ac0b8f5ffaba9b89017292
Laszlo Papp <djszapi@archlinux.us>
2009-12-12 19:27:41 +0100

    Fix the missing comment framework url in the urls.py
    Signed-off-by: Laszlo Papp <djszapi@archlinux.us>

--- archlinux/urls.py
+++ archlinux/urls.py
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
     (r'^users/(?P<user>\w+)/packages', 'aur.views.user_packages'),
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
+    (r'^comments/', include('django.contrib.comments.urls')),
 )
 
 if settings.DEBUG == True:
