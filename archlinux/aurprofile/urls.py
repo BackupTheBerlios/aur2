b387ea5e88928ccbf128293e71e05d8d419821ea
Laszlo Papp <djszapi@archlinux.us>
2009-12-13 21:51:30 +0100

    Change the loaded page for profile,packages in case /users/$username
    It's needed to remove the duplicated user details things, and keep the earlier
    existing one here.
    
    Signed-off-by: Laszlo Papp <djszapi@archlinux.us>

--- archlinux/aurprofile/urls.py
+++ archlinux/aurprofile/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('aurprofile.views',
-    url(r'^(?P<object_id>\w+)$', 'user_details',
+    url(r'^(?P<object_id>\w+)$', 'profile',
         name='aurprofile_profile'),
 )
